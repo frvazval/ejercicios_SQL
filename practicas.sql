5e6cdff5776b61cdafe8e1b037719ccdbada1c87
Fvazquez <foapgenerico@365.stucom.com>
2025-06-03 17:23:23 +0200

    stucom

--- practicas.sql
+++ practicas.sql
@@ -10,6 +10,8 @@ ventas_producto  int not null,
 id_proveedor int not null
 );
 
+alter table productos modify column ventas_producto int default 0;
+
 create table proveedores (
 id_proveedor int auto_increment primary key,
 nombre_proveedor varchar(50) not null
@@ -61,18 +63,24 @@ begin
     select id_producto into v_id_producto from productos where nombre_producto = p_nombre_producto;
     
     if v_id_producto is null then
-		
+		-- Si v_id_producto
+		insert into productos (nombre_producto, precio, stock_actual, id_proveedor) values
+        (p_nombre_producto, p_precio, p_stock, v_id_proveedor);
+        select concat_ws(" ", "Producto", p_nombre_producto, "añadido a la tabla");
     else
-    
-    end if;
-    
-    
-    
-    
+		update productos set precio = p_precio, stock_actual = stock_actual + p_stock
+        where id_producto = v_id_producto;
+        select concat_ws(" ", "Producto", p_nombre_producto, "actualizado");
+    end if;             
 end $$
 delimiter ;
 
 call insertar_productos ("Iphone 27", 5000.75, 2, "Apple");
+call insertar_productos ("Iphone 27", 6000.75, 3, "Apple");
+call insertar_productos ("S35", 1000, 5, "Samsung");
+call insertar_productos ("S35", 1000, 3, "Samsung");
+
+
 
 
 
